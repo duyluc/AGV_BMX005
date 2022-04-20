5ca640d5b93d064ee482f27317c4fe625170c405
duyluc <duongduyluc3@gmail.com>
2022-04-20 09:39:58 +0700

    backup

--- Test_mLib.py
+++ Test_mLib.py
@@ -56,9 +56,9 @@ def ProcessGyro(pretime):
             (x,y,z,t) = test_gyro85.GetGyroValue()
             if t:
                 dtime = timer() - pretime
-                gx_rate = (x - gyrooffsetX)
-                gy_rate = (y - gyrooffsetY)
-                gz_rate = (z - gyrooffsetZ)
+                gx_rate = (x - gyrooffsetX)/14.375
+                gy_rate = (y - gyrooffsetY)/14.375
+                gz_rate = (z - gyrooffsetZ)/14.375
 
                 anglegx = anglegx + (gx_rate * dtime)
                 anglegy = anglegy + (gy_rate * dtime)
